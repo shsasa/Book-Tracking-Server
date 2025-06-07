deeff2700c9df9908675b37d1ed7ccdb45d01886
noor96faisal <n.fai2@icloud.com>
2025-06-07 17:38:13 +0300

    User Ban BAckend

--- controllers/BannedBooks.js
+++ controllers/BannedBooks.js
@@ -1,6 +1,6 @@
 const BannedBooks = require('../models/BannedBooks')
 
-const bannedBook = async (req, res) => {
+const addBannedBook = async (req, res) => {
   try {
     const { user, book } = req.body
     const bannedBook = new BannedBooks({ user, book })
@@ -10,3 +10,24 @@ const bannedBook = async (req, res) => {
     res.status(400).send(`Error: ${error.message}`)
   }
 }
+
+const userBannedBook = async (req, res) => {
+  try {
+    const { user } = req.params
+    const bannedBooks = await BannedBooks.find({ user }).populate('book')
+    if (bannedBooks.length === 0)
+      return res.status(404).send(`No banned books found : ${user}`)
+
+    let responseText = `Banned Books for User ${user}:\n`
+    bannedBooks.forEach((entry) => {
+      responseText += `Books: ${entry.book
+        .map((b) => b.toString())
+        .join(', ')}\n`
+    })
+    res.status(200).send(responseText)
+  } catch (error) {
+    res.status(500).send(`Error: ${error.message}`)
+  }
+}
+
+module.export = { addBannedBook, userBannedBook }
